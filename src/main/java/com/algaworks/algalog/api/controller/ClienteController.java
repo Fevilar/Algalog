1b7ffab8d684936370d559802e65662aad5f4e04
Fevilar <fevilar@hotmail.com>
2022-01-21 21:27:58 -0300

    Modulo2

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,31 +13,41 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager manager;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		/* Com a nova versão do java, pode ser colocado o "var" para
-		 * declarar variáveis no lugar da class "Cliente".
-		 * Cliente cliente1 = Cliente();
-		 */
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Jorge Fernando");
-		cliente1.setTelefone("11 933328788");
-		cliente1.setEmail("Jorgef@hotmail.com");
-
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Antonio Sérgio");
-		cliente2.setTelefone("11 954684598");
-		cliente2.setEmail("aser68@msn.com");
-
-		var cliente3 = new Cliente();
-		cliente3.setId(3L);
-		cliente3.setNome("Maria da Silva");
-		cliente3.setTelefone("11 969578884");
-		cliente3.setEmail("maria.silva@gmail.com");
-
-		return Arrays.asList(cliente1, cliente2, cliente3);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
+
+	/*
+	 * Com a nova versão do java, pode ser colocado o "var" para declarar variáveis
+	 * no lugar da class "Cliente". Cliente cliente1 = Cliente();
+	 */
+//		Cliente cliente1 = new Cliente();
+//		cliente1.setId(1L);
+//		cliente1.setNome("Jorge Fernando");
+//		cliente1.setTelefone("11 93332-8788");
+//		cliente1.setEmail("Jorgef@hotmail.com");
+//
+//		Cliente cliente2 = new Cliente();
+//		cliente2.setId(2L);
+//		cliente2.setNome("Antonio Sérgio");
+//		cliente2.setTelefone("11 95468-4598");
+//		cliente2.setEmail("aser68@msn.com");
+//
+//		Cliente cliente3 = new Cliente();
+//		cliente3.setId(3L);
+//		cliente3.setNome("Maria da Silva");
+//		cliente3.setTelefone("11 96957-8884");
+//		cliente3.setEmail("maria.silva@gmail.com");
+//
+//		List<Cliente> lista = new ArrayList<Cliente>();
+//		lista.add(cliente1);
+//		lista.add(cliente2);
+//		lista.add(cliente3);
+//		
+//		return Arrays.asList(cliente1, cliente2, cliente3);
 
 }
--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,17 +1,37 @@
 package com.algaworks.algalog.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
 
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
 	private String nome;
+	
 	private String email;
+	
+	// Quando a coluna da tabela tem nome diferente do atributo da classe.
+	@Column(name = "fone")
 	private String telefone;
 	
+	
+	
+			
 	/* ****Lombok*****
 	 * incluir uma dependencia
 	 * botão dir no projeto->Spring->Add Starters->lombok
